50481e300161b6f250ca4bb1da2461d4592e8440
Aleš Rybák <ales.rybak@lundegaard.eu>
2019-10-21 14:20:42 +0200

    NOISSUE chores: Headers updated

--- src/main/java/eu/lundegaard/maven/selectivebuild/ChangesMojo.java
+++ src/main/java/eu/lundegaard/maven/selectivebuild/ChangesMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Lundegaard a.s., All Rights Reserved
+ * Copyright (C) 2019 Lundegaard a.s., All Rights Reserved
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
